0aaa289f4910114cca12b2d3acf604dcdbefbc7d
Jess Lê <jessicale90@gmail.com>
2022-09-05 22:20:40 -0600

    iOS-3: Display details of POI with image and name

--- MarinasApp/PointDetails/PointDetailsView.swift
+++ MarinasApp/PointDetails/PointDetailsView.swift
@@ -6,32 +6,70 @@
 //
 
 import Foundation
+import SafariServices
 import UIKit
 
 class PointDetailsView: UIView {
-    
-    var viewModel: PointDetailsViewModel?
+    let placeHolderImageURL = "https://picsum.photos/id/1084/200"
 
-    var imageView: UIImageView = {
+    var viewModel: PointDetailsViewModel? {
+        didSet {
+            configureUIDetails()
+        }
+    }
+
+    lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
-        imageView.isUserInteractionEnabled = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 2
+        imageView.image = UIImage(named: "notFound")
+
         return imageView
      }()
+
+    lazy var name: UILabel = {
+        let label = UILabel()
+        label.text = viewModel?.getName() ?? "N/A"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+     }()
+    
+    lazy var webButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("See More Info", for: .normal )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+     }()
+
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var detailStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.spacing = 16
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     override init(frame: CGRect){
         super.init(frame: frame)
 
-        self.layer.cornerRadius = 5
-        
-        let placeHolderImageURL = "https://picsum.photos/id/1084/200"
-        imageView.sd_setImage(with: URL(string: viewModel?.getImageName() ?? placeHolderImageURL), placeholderImage: UIImage(named: placeHolderImageURL)) { [weak self] (image, error, imageCacheType, imageUrl) in
-            guard let self = self else { return }
-            self.imageView.setNeedsDisplay()
-        }
-
-        self.addSubview(imageView)
+        addSubview(mainStackView)
+        mainStackView.addArrangedSubview(imageView)
+        mainStackView.addArrangedSubview(detailStackView)
+        detailStackView.addArrangedSubview(name)
 
         setImageLayoutConstraints()
     }
@@ -43,11 +81,19 @@ class PointDetailsView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func configureUIDetails() {
+        name.text = viewModel?.getName() ?? "N/A"
+        let imageName = viewModel?.getImageName()
+        imageView.sd_setImage(with: URL(string: imageName ?? placeHolderImageURL), placeholderImage: UIImage(named: placeHolderImageURL)) { [weak self] (image, error, imageCacheType, imageUrl) in
+            guard let self = self else { return }
+            self.imageView.setNeedsDisplay()
+        }
+    }
 
     private func setImageLayoutConstraints() {
-        imageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 12).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 12).isActive = true
+        mainStackView.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
     }
 }
